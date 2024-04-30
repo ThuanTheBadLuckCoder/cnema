18d90933ac010700ccc6025fef4dbd45377ef610
Tran Thuan <dtthuan.1211@gmail.com>
2024-05-01 03:22:29 +0700

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import img1 from './Group-17.png';
+import Navbar from './Components/Navbar/Navbar';
+import SearchingBar from './Components/Navbar/SearchingBar'
+import CardSlider from './Components/Slider/MultipleItems';
+import Cnema from './Pages/Cnema';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div class="Cnema-body">
-      <div id="carouselExample" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src={img1} class="d-block w-100" alt="test1" />
-    </div>
-    <div class="carousel-item">
-      <img src={img1} class="d-block w-100" alt="test1" />
-    </div>
-    <div class="carousel-item">
-      <img src={img1} class="d-block w-100" alt="test1" /> 
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+    <div>
+        <Navbar />
+        <SearchingBar />
+        <CardSlider />
     </div>
   );
 }
