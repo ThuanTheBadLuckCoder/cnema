42efc60318456c1f11845e80eb58fb5f1109e33b
Tran Thuan <dtthuan.1211@gmail.com>
2024-05-01 11:51:48 +0700

    fe
    coding frontend for mainpage

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import CNEMA from '../Assets/CNEMA.png'
-import cart_icon from '../Assets/cart_icon.png'
+
+import { Link } from 'react-router-dom';
+
+
 const Navbar = () => {
+
+  const [menu, setMenu] = useState("movies");
+
   return (
     <div className="navbar">
         <div className="navbar-container">
             <div className="nav-logo">
-                <img src={CNEMA} alt =""/>
+                <a href="Cnema"><img src={CNEMA} alt =""/></a>
             </div>
             <div className="nav-menu">
               <ul className="ul-nav-menu">
-                  <li><a href="#">Movies</a></li>
-                  <li><a href="#">News</a></li>
-                  <li><a href="#">Actors</a></li>
-                  <li><a href="#">More</a></li>
+                  <li onClick={()=>{setMenu("movies")}}><Link to='/Movies'>Movies</Link>{menu==="movies"?<></>:<></>}</li>
+                  <li onClick={()=>{setMenu("news")}}><Link to='/News'>News</Link>{menu==="news"?<></>:<></>}</li>
+                  <li onClick={()=>{setMenu("actors")}}><Link to='/Actors'>Actors</Link>{menu==="actors"?<></>:<></>}</li>
+                  <li onClick={()=>{setMenu("more")}}><Link to='/More'>More</Link>{menu==="more"?<></>:<></>}</li>
               </ul>
             </div>
             
 
             <div className="nav-login-button">
-            <button>Login</button>
+            <Link to='/LoginSignup'><button>Login</button></Link>
 
             </div>
         </div>
